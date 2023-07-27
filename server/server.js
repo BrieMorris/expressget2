7d1c05ada923d6739678fdb7be25bee5c5c6108e
G. A. Morris <129684030+gamor001@users.noreply.github.com>
2023-07-27 11:42:58 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,49 +1,54 @@
 let express = require('express');
 let app = express();
-const port = 5001
-app.use(express.json())
+const port = 5001;
 
-const quoteList = require('./quoteList')
+app.use(express.json());
+
+const quoteList = require('./quoteList.js');
 app.use(express.static('server/public'));
 
-//when we visit localhost:5001/quotes
-//in our browser, express will call this function
-app.get('/quotes', function(req, res){
+//Sets path and data to send back to client
+app.get('/quotes', function(req, res) {
     console.log('Request for /quotes was made');
+    res.send(quoteList);
+});
 
-    //send back the list of quotes
-    //so we can see it in our browser
-    res.send(quoteList)
-})
+app.post('/quotes', (req, res) => {
+    console.log('get a POST request', req.body);
 
-app.post('/quotes', (req,res) => {
-    console.log('request.', req);
-    console.log('get a POST request.', req.body);
-
-    let quote = req.body
-    quoteList.push(quote)
+    let quote = req.body;
+    quoteList.push(quote);
     res.sendStatus(201);
-})
+});
+
+app.delete('/quotes/:index', (req, res) => {
+    console.log('Delete request!', req.body);
+    console.log(req.params);//index #
+
+    let index = req.params.index;
+    //delete quoteList[index]; leaves null value in its place - do not use to send to database
+    quoteList.splice(index, index);
+    res.sendStatus(201);
+});
 
 
-app.listen(port, function() { 
-    console.log('listening on port', port);
-})
-
-// http://localhost:5001/quotes
-//  this is called | Route | Path | URL
-//
-// 4 types of routes
-// GET
-// POST
-// PUT
-// DELETE
-// CRUD - Create, Read, Update, Delete
 
 
-// status codes 
-// 200 Ok - everthing worked as expected.
-// 201 Created - a record was created without issue.
-// 400 Bad Request - something went wrong in your route.
-// 404 Not found - you probobly have the wrong address
-// 500 Server Error - something bad happended on the server
+app.listen(port, function() {
+    console.log('listening on port', port)
+});
+
+//4 types of routes:
+//get
+//post
+//put
+//delete
+//CRUD - Create Read Update Delete
+
+//status codes:
+//server request always includes status codes
+// 200 - Okay - everything worked as expected
+// 201 - Created - record was created without issue
+// 400 - Bad Request - something went wrong in your route
+// 404 - Not found
+// 500 - Server Error - something bad happened on the server
